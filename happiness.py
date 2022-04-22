96050514e796297db2a1610e736fdc9b6d36b781
kel-kong <kkong@ucsd.edu>
2022-04-22 14:12:09 -0700

    testing

--- happiness.py
+++ happiness.py
@@ -44,7 +44,7 @@ USER = toml_data['mysql']['user']
 PORT = toml_data['mysql']['port']
 
 conn = pymysql.connect(
-    host=HOST_NAME,
+    host='127.0.0.1',
     port=PORT,
     user=USER,
     passwd=PASSWORD,

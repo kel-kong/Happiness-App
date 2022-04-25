c1612997fea6b035e31c24e394023e33c68aaf69
kel-kong <kkong@ucsd.edu>
2022-04-25 12:25:33 -0700

    testing

--- happiness.py
+++ happiness.py
@@ -11,14 +11,14 @@ import pymysql.cursors
 
 #Initialize connection.
 #Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
-def init_connection():
-    return  mysql.connector.connect(host="localhost", port = 3306, user="root",
-        password="mango123",
-        database="Happiness"
-        )
+# @st.experimental_singleton
+# def init_connection():
+#     return  mysql.connector.connect(host="localhost", port = 3306, user="root",
+#         password="mango123",
+#         database="Happiness"
+#         )
 
-conn = init_connection()
+# conn = init_connection()
 
 # # Perform query.
 # # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
@@ -57,22 +57,25 @@ conn = init_connection()
 
 # makes the slider for the user to choose country, starting year, ending year, and/or region
 # to get country options: get unique values of countries
-# countries_df = pd.read_sql_query("SELECT DISTINCT `Country` FROM `happiness`.`2015` ORDER BY `Country` ASC;", conn)
-# countries_list = df['Country'].to_list()
-# country = st.selectbox("Select a country:", countries_list)
+country_region = st.radio("Choose your player:", ['Country', 'Region'])
+if country_region == "Country":
+	countries_df = pd.read_sql_query("SELECT DISTINCT `Country` FROM `happiness`.`2015` ORDER BY `Country` ASC;", conn)
+	countries_list = df['Country'].to_list()
+	country = st.selectbox("Select a country:", countries_list)
+	st.write("Country", country)
+	
+elif country_region == "Region":
+	regions_df = pd.read_sql_query("SELECT DISTINCT `Region` FROM `happiness`.`2015` ORDER BY `Region` ASC;", conn)
+	regions_list = regions_df['Region'].to_list()
+	regions_list.append("None")
+	region = st.selectbox("Select a region to get the average:", regions_list)
+	st.write("Region", region)
 
-# regions_df = pd.read_sql_query("SELECT DISTINCT `Region` FROM `happiness`.`2015` ORDER BY `Region` ASC;", conn)
-# regions_list = regions_df['Region'].to_list()
-# regions_list.append("None")
-# region = st.selectbox("Select a region to get the average:", regions_list)
+starting_year = st.slider("Select starting year:", min_value = 2015, max_value = 2019, step = 1)
+ending_year = st.slider("Select ending year:", min_value = 2015, max_value = 2019, step = 1)
 
-# starting_year = st.slider("Select starting year:", min_value = 2015, max_value = 2019, step = 1)
-# ending_year = st.slider("Select ending year:", min_value = 2015, max_value = 2019, step = 1)
-
-# st.write("Country", country)
-# st.write("Region", region)
-# st.write("Starting Year", starting_year)
-# st.write("Ending Year", ending_year)
+st.write("Starting Year", starting_year)
+st.write("Ending Year", ending_year)
 
 # if region =! 'None':
 # 	query = "SELECT happiness.`" + starting_year + "`.`Happiness Rank` - happiness.`" + ending_year + "`.`Happiness Rank` AS 'Happiness Rank Difference' FROM happiness.`" + ending_year + "` INNER JOIN happiness.`" + starting_year + "` ON happiness.`" + ending_year + "`.`Country` = happiness.`" + starting_year + "`.`Country` WHERE happiness.`" + ending_year + "`.`Country` = '" + country + "'"

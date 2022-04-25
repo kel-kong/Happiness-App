3cd982103e209aa392fa8b570e3b8659995257eb
kel-kong <kkong@ucsd.edu>
2022-04-25 13:28:03 -0700

    testing

--- happiness.py
+++ happiness.py
@@ -55,28 +55,30 @@ import pymysql.cursors
 #     conn)
 # df.tail(10)
 
+# TITLE 
+st.title("Kelly's Function **but** Non-Functional Happiness App")
 # makes the slider for the user to choose country, starting year, ending year, and/or region
 # to get country options: get unique values of countries
 country_region = st.radio("Choose your player:", ['Country', 'Region'])
 if country_region == "Country":
 	# countries_df = pd.read_sql_query("SELECT DISTINCT `Country` FROM `happiness`.`2015` ORDER BY `Country` ASC;", conn)
 	# countries_list = df['Country'].to_list()
-	countries_list = ['Deez', 'Nuts']
+	countries_list = ['testing', '1', '2', '3']
 	country = st.selectbox("Select a country:", countries_list)
-	st.write("Country", country)
+	st.write("Country:", country)
 
 elif country_region == "Region":
 	# regions_df = pd.read_sql_query("SELECT DISTINCT `Region` FROM `happiness`.`2015` ORDER BY `Region` ASC;", conn)
 	# regions_list = regions_df['Region'].to_list()
-	regions_list = ['Ima', 'Break', 'Down']
+	regions_list = ['testing', '1', '2', '3']
 	region = st.selectbox("Select a region to get the average:", regions_list)
-	st.write("Region", region)
+	st.write("Region:", region)
 
 starting_year = st.slider("Select starting year:", min_value = 2015, max_value = 2019, step = 1)
 ending_year = st.slider("Select ending year:", min_value = 2015, max_value = 2019, step = 1)
 
-st.write("Starting Year", starting_year)
-st.write("Ending Year", ending_year)
+st.write("Starting Year:", starting_year)
+st.write("Ending Year:", ending_year)
 
 # if region =! 'None':
 # 	query = "SELECT happiness.`" + starting_year + "`.`Happiness Rank` - happiness.`" + ending_year + "`.`Happiness Rank` AS 'Happiness Rank Difference' FROM happiness.`" + ending_year + "` INNER JOIN happiness.`" + starting_year + "` ON happiness.`" + ending_year + "`.`Country` = happiness.`" + starting_year + "`.`Country` WHERE happiness.`" + ending_year + "`.`Country` = '" + country + "'"

c9b5498ec829aff9e72fe938c3c2a3c60d9f2912
kel-kong <kkong@ucsd.edu>
2022-04-24 21:44:56 -0700

    slider

--- happiness.py
+++ happiness.py
@@ -11,14 +11,14 @@ import pymysql.cursors
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
-def init_connection():
-    return  mysql.connector.connect(host="localhost", port = 3306, user="root",
-        password="mango123",
-        database="happiness"
-        )
+# @st.experimental_singleton
+# def init_connection():
+#     return  mysql.connector.connect(host="localhost", port = 3306, user="root",
+#         password="mango123",
+#         database="happiness"
+#         )
 
-conn = init_connection()
+# conn = init_connection()
 
 # # Perform query.
 # # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
@@ -55,6 +55,12 @@ conn = init_connection()
 #     conn)
 # df.tail(10)
 
+# makes the slider for the user to choose country, starting year, ending year, and/or region
+# to get country options: get unique values of countries
+# countries_df = pd.read_sql_query("SELECT DISTINCT `Country` FROM `happiness`.`2015`", conn)
+# countries_list = df['Country'].to_list()
+countries_list = ['H', 'D', 'F']
+country = st.selectbox("Select a country:", countries_list)
 starting_year = st.slider("Select starting year:", min_value = 2015, max_value = 2019, step = 1)
 ending_year = st.slider("Select ending year:", min_value = 2015, max_value = 2019, step = 1)
 st.write("Starting Year", starting_year)

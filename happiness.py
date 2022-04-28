cfee3eeecfbc813646bb388ec77d23dfddeb9fee
kel-kong <kkong@ucsd.edu>
2022-04-27 21:40:24 -0700

    testing

--- happiness.py
+++ happiness.py
@@ -1,7 +1,6 @@
 # happiness.py
 
 import streamlit as st
-import mysql.connector
 import pandas as pd
 import altair as alt
 import os
@@ -9,51 +8,25 @@ import toml
 import pymysql.cursors
 
 
-#Initialize connection.
-#Uses st.experimental_singleton to only run once.
-# @st.experimental_singleton
-# def init_connection():
-#     return  mysql.connector.connect(host="localhost", port = 3306, user="root",
-#         password="mango123",
-#         database="Happiness"
-#         )
+# Reading data
+toml_data = toml.load("secrets.toml")
+# saving each credential into a variable
+HOST_NAME = toml_data['mysql']['host']
+DATABASE = toml_data['mysql']['database']
+PASSWORD = toml_data['mysql']['password']
+USER = toml_data['mysql']['user']
+PORT = toml_data['mysql']['port']
 
-# conn = init_connection()
+conn = pymysql.connect(
+    host=HOST_NAME, #"localhost"
+    port=int(3306), 
+    user=USER, #"root"
+    password=PASSWORD,
+    db=DATABASE, #"Happiness"
+    cursorclass=pymysql.cursors.DictCursor)
 
-# # Perform query.
-# # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-# @st.experimental_memo(ttl=600)
-# def run_query(query):
-#     with conn.cursor() as cur:
-#         cur.execute(query)
-#         return cur.fetchall()
-
-# rows = run_query("SELECT * FROM `2015`;")
-
-# # Print results.
-# for row in rows:
-#     st.write(f"{row[0]} has a :{row[1]}:")
-
-# # Reading data
-# toml_data = toml.load("secrets.toml")
-# # saving each credential into a variable
-# HOST_NAME = toml_data['mysql']['host']
-# DATABASE = toml_data['mysql']['database']
-# PASSWORD = toml_data['mysql']['password']
-# USER = toml_data['mysql']['user']
-# PORT = toml_data['mysql']['port']
-
-# conn = pymysql.connect(
-#     host=HOST_NAME,
-#     port=int(3306),
-#     user=USER,
-#     password=PASSWORD,
-#     db=DATABASE,
-#     cursorclass=pymysql.cursors.DictCursor)
-
-# df = pd.read_sql_query("SELECT * FROM `2015`",
-#     conn)
-# df.tail(10)
+df = pd.read_sql_query("SELECT * FROM happiness.`2015`", conn)
+df.tail(10)
 
 # TITLE 
 st.title("Happiness")
